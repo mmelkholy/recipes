b6b8d293c19e0285586067f8a31d8842e0a086c8
Mohamed Elkholy <mmelkholy@gmail.com>
2021-05-26 19:15:18 +0300

    Last point in Directives

---
+++ src/app/shared/drop-down.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DropdownDirective } from './drop-down.directive';
+
+describe('DropDownDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DropdownDirective();
+    expect(directive).toBeTruthy();
+  });
+});
